8b9fde85259d0ba35ab6f15e93fe632d5f555e3c
KitchenGun <kitchengun124@gmail.com>
2022-07-29 23:53:26 +0900

    listen서버 구현을 위해서 기존 게임의 코드 재활용

--- Source/PanicRoom/CreateGameInstance.cpp
+++ Source/PanicRoom/CreateGameInstance.cpp
@@ -1,26 +1,78 @@
 
 #include "CreateGameInstance.h"
 #include "Kismet/GameplayStatics.h"
+#include "GameFramework/PlayerState.h"
 #include "Engine/World.h"
 
 void UCreateGameInstance::Init()
 {
+	//플레이 시작할때 
 	Super::Init();
-
-	if (IOnlineSubsystem* SubSystem = IOnlineSubsystem::Get())
+	//server
+	if (IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get())
 	{
-		SessionInterface = SubSystem->GetSessionInterface();
+		SessionInterface = Subsystem->GetSessionInterface();
 		if (SessionInterface.IsValid())
 		{
-			SessionInterface->OnCreateSessionCompleteDelegates.AddUObject(this,&UCreateGameInstance::OnCreateSessionComplete);
-			SessionInterface->OnFindSessionsCompleteDelegates.AddUObject(this,&UCreateGameInstance::OnFindSessionComplete);
+			//Bind Delegates
+			SessionInterface->OnCreateSessionCompleteDelegates.AddUObject(this, &UCreateGameInstance::OnCreateSessionComplete);
+			SessionInterface->OnFindSessionsCompleteDelegates.AddUObject(this, &UCreateGameInstance::OnFindSessionComplete);
 			SessionInterface->OnJoinSessionCompleteDelegates.AddUObject(this, &UCreateGameInstance::OnJoinSessionComplete);
 		}
 	}
 }
 
-void UCreateGameInstance::OnCreateSessionComplete(FName SessionName, bool Succeeded)
+void UCreateGameInstance::CreateServer()
 {
+	FOnlineSessionSettings SessionSettings;
+	SessionSettings.bAllowJoinInProgress = false;///수정함
+	SessionSettings.bUseLobbiesIfAvailable = true;//수정함
+	SessionSettings.bIsDedicated = false;
+	if (IOnlineSubsystem::Get()->GetSubsystemName() != "NULL")
+		SessionSettings.bIsLANMatch = false;
+	else
+		SessionSettings.bIsLANMatch = true;
+
+	SessionSettings.bShouldAdvertise = true;
+	SessionSettings.bUsesPresence = true;
+	SessionSettings.NumPublicConnections = 8;
+	SessionSettings.Set(FName("SESSION_NAME"),
+		UGameplayStatics::GetPlayerController(GetWorld(), 0)->PlayerState->GetPlayerName(),
+		EOnlineDataAdvertisementType::ViaOnlineService);
+
+	//SessionSettings.Settings.Add(FName("SESSION_NAME"),
+	//	UGameplayStatics::GetPlayerController(GetWorld(),0)->PlayerState->GetPlayerName());
+
+	SessionInterface->CreateSession(0,
+		FName(UGameplayStatics::GetPlayerController(GetWorld(), 0)->PlayerState->GetPlayerName()), SessionSettings);
+}
+
+
+void UCreateGameInstance::JoinServer(FServerInfo Info)
+{
+	SessionInterface->JoinSession(0, FName(Info.ServerName), SearchResults[Info.Index]);
+}
+
+void UCreateGameInstance::FindServer()
+{
+	//서버 결과 초기화
+	SearchResults.Empty();
+
+	SessionSearch = MakeShareable(new FOnlineSessionSearch());
+	if (IOnlineSubsystem::Get()->GetSubsystemName() != "NULL")
+		SessionSearch->bIsLanQuery = false;
+	else
+		SessionSearch->bIsLanQuery = true;//isLan
+	SessionSearch->MaxSearchResults = 10000;
+	SessionSearch->QuerySettings.Set(SEARCH_PRESENCE, true, EOnlineComparisonOp::Equals);
+
+	SessionInterface->FindSessions(0, SessionSearch.ToSharedRef());
+}
+
+
+void UCreateGameInstance::OnCreateSessionComplete(FName ServerName, bool Succeeded)
+{
+	UE_LOG(LogTemp, Display, L"OnCreateSessionComplete, Succeeded %d", Succeeded);
 	if (Succeeded)
 	{
 		GetWorld()->ServerTravel("/Game/Level/TestMap?listen");
@@ -29,55 +81,36 @@ void UCreateGameInstance::OnCreateSessionComplete(FName SessionName, bool Succee
 
 void UCreateGameInstance::OnFindSessionComplete(bool Succeeded)
 {
+	UE_LOG(LogTemp, Display, L"OnFindSessionComplete, Succeeded %d", Succeeded);
 	if (Succeeded)
 	{
-		TArray<FOnlineSessionSearchResult> SearchResults = SessionSearch->SearchResults;
-
-		if (SearchResults.Num())
+		SearchResults = SessionSearch->SearchResults;
+		int32 index = 0;
+		for (FOnlineSessionSearchResult Result : SearchResults)
 		{
-			//	강제로 0번 세션에 집어 넣기 수정 필요
-			SessionInterface->JoinSession(0,FName("Find Session"),SearchResults[0]);
+			if (!Result.IsValid())
+				continue;
+			FServerInfo Info;
+			Info.ServerName = Result.Session.SessionSettings.Settings.FindRef("SESSION_NAME").Data.ToString();//서버 이름
+			Info.MaxPlayers = Result.Session.SessionSettings.NumPublicConnections; //최대인원
+			Info.CurrentPlayers = Info.MaxPlayers - Result.Session.NumOpenPublicConnections;//현재인원
+			Info.Index = index;
+			//delegate 호출
+			if (FServerListDel.IsBound())
+				FServerListDel.Broadcast(Info);
+			index++;
 		}
 	}
 }
 
 void UCreateGameInstance::OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result)
 {
+	UE_LOG(LogTemp, Warning, L"OnJoinSessionComplete , sessionName %s", *SessionName.ToString());
 	if (APlayerController* PC = UGameplayStatics::GetPlayerController(GetWorld(), 0))
 	{
-		FString JoinAddress ="";
-		SessionInterface->GetResolvedConnectString(SessionName,JoinAddress);
+		FString JoinAddress = "";
+		SessionInterface->GetResolvedConnectString(SessionName, JoinAddress);
 		if (JoinAddress != "")
-		{
-			PC->ClientTravel(JoinAddress,ETravelType::TRAVEL_Absolute);
-		}
+			PC->ClientTravel(JoinAddress, ETravelType::TRAVEL_Absolute);
 	}
-}
-
-void UCreateGameInstance::CreateServer()
-{
-	UE_LOG(LogTemp, Warning, TEXT("CreateServer"));
-	FOnlineSessionSettings SessionSettings;
-	SessionSettings.bAllowJoinInProgress = true;
-	SessionSettings.bIsDedicated = false;
-	if (IOnlineSubsystem::Get()->GetSubsystemName() != "NULL")
-		SessionSettings.bIsLANMatch = false;
-	else
-		SessionSettings.bIsLANMatch = true;
-	SessionSettings.bShouldAdvertise = true;
-	SessionSettings.bUsesPresence = true;
-	SessionSettings.NumPublicConnections = 5;
-
-	SessionInterface->CreateSession(0, FName("Crete Session"), SessionSettings);
-
-}
-
-void UCreateGameInstance::JoinServer()
-{
-	SessionSearch = MakeShareable(new FOnlineSessionSearch());
-	SessionSearch->bIsLanQuery = (IOnlineSubsystem::Get()->GetSubsystemName() =="NULL");
-	SessionSearch->MaxSearchResults = 100000;
-	SessionSearch->QuerySettings.Set("SEARCH_PRESENCE",true,EOnlineComparisonOp::Equals);
-
-	SessionInterface->FindSessions(0, SessionSearch.ToSharedRef());
 }
--- Source/PanicRoom/CreateGameInstance.h
+++ Source/PanicRoom/CreateGameInstance.h
@@ -7,23 +7,52 @@
 #include "Engine/GameInstance.h"
 #include "CreateGameInstance.generated.h"
 
+
+USTRUCT(BlueprintType)
+struct FServerInfo
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(BlueprintReadOnly)
+		FString ServerName;
+	UPROPERTY(BlueprintReadOnly)
+		int32 CurrentPlayers;
+	UPROPERTY(BlueprintReadOnly)
+		int32 MaxPlayers;
+	UPROPERTY(BlueprintReadOnly)
+		int32 Index;
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FServerDel, FServerInfo, FServerInfoDel);
+
 UCLASS()
 class PANICROOM_API UCreateGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
 	
-protected:
-	TSharedPtr<FOnlineSessionSearch> SessionSearch;
-	IOnlineSessionPtr SessionInterface;
+public:
 
-	virtual void Init() override;
-
-	virtual void OnCreateSessionComplete(FName SessionName,bool Succeeded);
-	virtual void OnFindSessionComplete(bool Succeeded);
-	virtual void OnJoinSessionComplete(FName SessionName,EOnJoinSessionCompleteResult::Type Result);
-
+	//server
 	UFUNCTION(BlueprintCallable)
 	void CreateServer();
 	UFUNCTION(BlueprintCallable)
-	void JoinServer();
+	void JoinServer(FServerInfo Info);
+	UFUNCTION(BlueprintCallable)
+	void FindServer();
+
+	//µ®∏Æ∞‘¿Ã∆Æ
+	UPROPERTY(BlueprintAssignable)
+	FServerDel FServerListDel;
+
+protected:
+	virtual void Init() override;
+	//server
+	virtual void OnCreateSessionComplete(FName ServerName, bool Succeeded);
+	virtual void OnFindSessionComplete(bool Succeeded);
+	virtual void OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
+
+	IOnlineSessionPtr SessionInterface;
+	TSharedPtr<FOnlineSessionSearch> SessionSearch;
+	TArray<FOnlineSessionSearchResult> SearchResults;
+
 };
